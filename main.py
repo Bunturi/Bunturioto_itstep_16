8be27d07ab10653588027ed7f21af0e332109521
bunturioto <Bunturioto@gmail.com>
2024-02-09 22:45:14 +0400

    add class BankAccount for Homework 16.1

---
+++ main.py
@@ -0,0 +1,6 @@
+# Homework N16.1
+class BankAccount:
+    def __init__(self, account_number, account_holder, balance):
+        self.account_number = account_number
+        self.account_holder = account_holder
+        self.balance = balance
